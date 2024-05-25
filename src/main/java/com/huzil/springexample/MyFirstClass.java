6c11e68214e5d6a91481377a5a38d79768c237ee
Konstantyn <gagarin2789@gmail.com>
2024-05-25 21:33:56 +0200

    Provided variable to constructor

--- src/main/java/com/huzil/springexample/MyFirstClass.java
+++ src/main/java/com/huzil/springexample/MyFirstClass.java
@@ -1,12 +1,17 @@
 package com.huzil.springexample;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstClass {
 
+    private String myVar;
+
+    public MyFirstClass(String myVar) {
+        this.myVar = myVar;
+    }
+
     public String sayHello() {
-        return "Hello World";
+        return "Hello World from MyFirstClass ===> " + myVar;
     }
 }
