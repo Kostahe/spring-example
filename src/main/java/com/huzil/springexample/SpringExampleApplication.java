1de8c3535299cb1cb1900ed7369b6eb851f8cf80
Konstantyn <gagarin2789@gmail.com>
2024-05-25 19:00:15 +0200

    Tested app context in spring

--- src/main/java/com/huzil/springexample/SpringExampleApplication.java
+++ src/main/java/com/huzil/springexample/SpringExampleApplication.java
@@ -2,12 +2,21 @@ package com.huzil.springexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringExampleApplication.class, args);
+        var context = SpringApplication.run(SpringExampleApplication.class, args);
+
+        MyFirstClass myFirstClass = context.getBean(MyFirstClass.class);
+        System.out.println(myFirstClass.sayHello());
+    }
+
+    @Bean
+    public MyFirstClass myFirstClass() {
+        return new MyFirstClass();
     }
 
 }
