5d29c2d99bf8fdd61be35d20e31c01ce831ef3b5
Konstantyn <gagarin2789@gmail.com>
2024-05-25 19:09:16 +0200

    Tested @Service, @Component, @Repository annotations

--- src/main/java/com/huzil/springexample/MyFirstClass.java
+++ src/main/java/com/huzil/springexample/MyFirstClass.java
@@ -1,5 +1,9 @@
 package com.huzil.springexample;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MyFirstClass {
 
     public String sayHello() {
